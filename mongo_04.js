0909569fa74ac66f6e9342babc1bd48cb118a120
HaQadosch <haqadosch@gmail.com>
2015-11-21 19:31:41 +0000

    Running mocha tests with gulp

--- mongo_04.js
+++ mongo_04.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 describe('my feature', function() {
   it('works', function() {
-    assert.equal('A', 'A');
+    assert.equal('B', 'A');
   });
 
   it('fails gracefully', function() {
